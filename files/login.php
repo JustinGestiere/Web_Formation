fe2951c5e25518d30c3f27b861424186c64c8326
JustinG <justin.gestiere@gmail.com>
2024-11-12 10:42:28 +0100

    tout

--- files/login.php
+++ files/login.php
@@ -1,31 +1,3 @@
-<?php
-// Inclure le fichier de connexion
-require 'bdd.php';
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $email = $_POST['email']; // Changez ici pour utiliser le même nom que dans le formulaire
-    $password = $_POST['password'];
-
-    // Préparation de la requête pour vérifier l'email
-    $sql = "SELECT * FROM users WHERE emails = :email"; // Assurez-vous que cela correspond au nom de votre colonne
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':email', $email);
-    $stmt->execute();
-
-    $user = $stmt->fetch();
-
-    if ($user && password_verify($password, $user['passwords'])) { // Utiliser 'passwords' pour le champ de mot de passe
-        // Connexion réussie
-        session_start();
-        $_SESSION['user_id'] = $user['id'];
-        header("Location: accueil.php"); // Redirige vers la page d'accueil
-        exit;
-    } else {
-        echo "Identifiants incorrects";
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -36,43 +8,71 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="../css/authentification.css" rel="stylesheet">
 </head>
-<body>
-    <header>
-        <div class="image">
-            <img src="../images/logo.jpg" alt="Logo du site WEB FORMATION" class="logo">
-            <h2 class="h2_logo">WEB FORMATION</h2>
-        </div>
-    </header>
+    <body>
+        <?php
+            session_start();
+            // Inclure le fichier de connexion
+            require 'bdd.php';
 
-    <main>
-        <form method="post" action="login.php" class="p-4 border border-light rounded">
-            <div class="form-group">
-                <label for="email">Adresse mail :</label>
-                <input type="email" class="form-control" id="email" name="email" required 
-                value="<?php echo isset($_COOKIE['email']) ? htmlspecialchars($_COOKIE['email']) : ''; ?>"> <!-- Préremplir avec le cookie -->
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Vérifie la requête HTTP reçue est de type POST, c'est à dire que le formulaire a été soumis
+                $email = $_POST['email']; 
+                $password = $_POST['password'];
+
+                // Verifie avec la bdd si le user existe
+                $sql = "SELECT * FROM users WHERE emails = :email"; // verifie si l'email existe dans la bdd
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindParam(':email', $email);
+                $stmt->execute();
+                $user = $stmt->fetch();
+
+                //
+                if ($user && password_verify($password, $user['passwords'])) { // Utiliser 'passwords' pour le champ de mot de passe
+                    // Connexion réussie
+                    $_SESSION['user_id'] = $user['id'];
+                    header("Location: index.php"); // Redirige vers la page d'accueil
+                    exit;
+                } else {
+                    echo "Identifiants incorrects";
+                }
+            }
+        ?>
+
+        <header>
+            <div class="image">
+                <img src="../images/logo.jpg" alt="Logo du site WEB FORMATION" class="logo">
+                <h2 class="h2_logo">WEB FORMATION</h2>
             </div>
+        </header>
 
-            <div class="form-group">
-                <label for="password">Mot de passe :</label>
-                <input type="password" class="form-control" id="password" name="password" required>
-            </div>
+        <main>
+            <form method="post" action="login.php" class="p-4 border border-light rounded">
+                <div class="form-group">
+                    <label for="email">Adresse mail :</label>
+                    <input type="email" class="form-control" id="email" name="email" required 
+                    value="<?php echo isset($_COOKIE['email']) ? htmlspecialchars($_COOKIE['email']) : ''; ?>"> <!-- Préremplir avec le cookie -->
+                </div>
 
-            <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" name="remember" value="1" id="remember">
-                <label class="form-check-label" for="remember">Se souvenir de moi</label>
-            </div>
+                <div class="form-group">
+                    <label for="password">Mot de passe :</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
 
-            <button type="submit" class="btn btn-primary">Connexion</button>
-            <!-- <div class="form-group">
-                <a href="forgot_password.php" class="text-secondary">Mot de passe oublié ?</a>
-            </div> -->
-            <button type="button" class="btn btn-secondary" onclick="window.location.href='register.php';">S'enregistrer</button>
-        </form>
-    </main>
-    <!-- Scripts de Bootstrap -->
-    <script src="../js/main.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" name="remember" value="1" id="remember">
+                    <label class="form-check-label" for="remember">Se souvenir de moi</label>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Connexion</button>
+                <!-- <div class="form-group">
+                    <a href="forgot_password.php" class="text-secondary">Mot de passe oublié ?</a>
+                </div> -->
+                <button type="button" class="btn btn-secondary" onclick="window.location.href='register.php';">S'enregistrer</button>
+            </form>
+        </main>
+        <!-- Scripts de Bootstrap -->
+        <script src="../js/main.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    </body>
 </html>
