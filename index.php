493a9ec8f5f417e8eb239a8be1dcffb7acb96ef1
JustinG <justin.gestiere@gmail.com>
2025-03-10 17:41:28 +0100

    test

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (isset($_SESSION['user_role'])) {
 }
 
 if (file_exists(__DIR__ . "/css/accueil.css")) {
-    echo "Le fichier CSS existe !";
+    echo "Le fichier CSS existeeeeeee !";
 } else {
     echo "Le fichier CSS est introuvable.";
 }
