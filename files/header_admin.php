23a0f5fe62c7d51baa0f708dff22dfbdf11f51a5
JustinG <justin.gestiere@gmail.com>
2025-03-16 12:57:03 +0100

    header admin

--- files/header_admin.php
+++ files/header_admin.php
@@ -46,7 +46,7 @@ try {
             <nav>
                 <ul class="nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php">Accueil</a>
+                        <a class="nav-link" href="../index.php">Accueil</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="matieres.php">Matières</a>
