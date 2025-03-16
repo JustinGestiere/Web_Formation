46cbddc8beae8f10530620b9be2ba9ab9fabbf04
JustinG <justin.gestiere@gmail.com>
2025-03-16 13:06:11 +0100

    test

--- files/header.php
+++ files/header.php
@@ -7,25 +7,25 @@ if (isset($_SESSION['user_role'])) {
     $user_role = $_SESSION['user_role'];
 } else {
     // Si l'utilisateur n'est pas connecté, redirige vers login.php
-    header("Location: login.php");
+    header("Location: files/login.php");
     exit();
 }
 
 // En fonction du rôle, inclure le bon header
 switch ($user_role) {
     case 'admin':
-        include('header_admin.php');
+        include('files/header_admin.php');
         break;
     case 'prof':
-        include('header_prof.php');
+        include('files/header_prof.php');
         break;
     case 'eleve':
     case 'visiteur':
-        include('header_eleve.php'); // Header générique pour élèves et visiteurs
+        include('files/header_eleve.php'); // Header générique pour élèves et visiteurs
         break;
     default:
         // Si aucun rôle valide, rediriger vers login.php
-        header("Location: login.php");
+        header("Location: files/login.php");
         exit();
 }
 ?>
