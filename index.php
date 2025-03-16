fb4de34209a229f0e5d679f290e797c3ffa0c50d
JustinG <justin.gestiere@gmail.com>
2025-03-16 12:35:49 +0100

    index

--- index.php
+++ index.php
@@ -5,28 +5,20 @@ session_start(); // Démarre la session si ce n'est pas déjà fait
 if (isset($_SESSION['user_role'])) {
     switch ($_SESSION['user_role']) {
         case 'admin':
-            include "header_admin.php"; // Si rôle admin
+            include "files/header_admin.php"; // Si rôle admin
             break;
         case 'prof':
-            include "header_prof.php"; // Si rôle prof
+            include "files/header_prof.php"; // Si rôle prof
             break;
         default:
-            include "header.php"; // Sinon le header par défaut
+            include "files/header.php"; // Sinon le header par défaut
             break;
     }
 } else {
     // Si l'utilisateur n'est pas connecté, on peut rediriger vers login
-    header("Location: ../files/login.php");
+    header("Location: files/login.php");
     exit();
 }
-
-if (file_exists(__DIR__ . "/css/accueil.css")) {
-    echo "Le fichier CSS existe !";
-} else {
-    echo "Le fichier CSS est introuvable.";
-}
-
-
 ?>
 
 <!DOCTYPE html>
@@ -264,5 +256,5 @@ foreach ($days as $day) {
 </html>
 
 <?php
-  include "footer.php";
+  include "files/footer.php";
 ?>
