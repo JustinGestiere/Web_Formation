c0d00d6210e5dc3d47276b4291f5e78a7177bd10
JustinG <justin.gestiere@gmail.com>
2025-03-16 13:07:50 +0100

    test2

--- files/classes.php
+++ files/classes.php
@@ -1,5 +1,24 @@
 <?php
-include_once 'files/header.php';
+session_start(); // Démarre la session si ce n'est pas déjà fait
+
+// Inclure le header approprié en fonction du rôle
+if (isset($_SESSION['user_role'])) {
+    switch ($_SESSION['user_role']) {
+        case 'admin':
+            include "files/header_admin.php"; // Si rôle admin
+            break;
+        case 'prof':
+            include "files/header_prof.php"; // Si rôle prof
+            break;
+        default:
+            include "files/header.php"; // Sinon le header par défaut
+            break;
+    }
+} else {
+    // Si l'utilisateur n'est pas connecté, on peut rediriger vers login
+    header("Location: login.php");
+    exit();
+}
 $message="";
 ?>
 
