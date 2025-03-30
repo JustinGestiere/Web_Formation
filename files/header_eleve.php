c6a4f3ca5919997e9a3ec8a4b3e685944dbe9fd1
JustinG <justin.gestiere@gmail.com>
2025-03-30 18:54:46 +0200

    eleve

--- files/header_eleve.php
+++ files/header_eleve.php
@@ -107,50 +107,6 @@ try {
         <li class="nav-item">
             <a class="nav-link" href="eleve.php">Accueil</a>
         </li>
-<?php
-if (!isset($_SESSION)) {
-    session_start();
-}
-?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Interface Élève</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="eleve.php">Web Formation</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="eleve.php">
-                            <i class="fas fa-home"></i> Accueil
-                        </a>
-                    </li>
-                </ul>
-                <?php if (isset($_SESSION['user_id'])): ?>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <form method="post" action="logout.php" class="d-inline">
-                                <button type="submit" class="btn btn-link nav-link">
-                                    <i class="fas fa-sign-out-alt"></i> Déconnexion
-                                </button>
-                            </form>
-                        </li>
-                    </ul>
-                <?php endif; ?>
-            </div>
-        </div>
-    </nav>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <?php if (isset($_SESSION['user_id'])): ?>
             <li class="nav-item">
                 <form method="post" action="/files/logout.php" class="d-inline">
